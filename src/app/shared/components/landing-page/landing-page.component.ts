b419ee5892627b467ba8ddbe1e580d4f264d2fd9
Ganapathy Subramanian S <ganapathys@presidio.com>
2022-09-10 19:20:48 +0530

    landing-page done

--- src/app/shared/components/landing-page/landing-page.component.ts
+++ src/app/shared/components/landing-page/landing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import myAppConfig from 'src/app/core/config/my-app-config';
+import { LandingService } from 'src/app/core/services/landing.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,7 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  postersList:any;
+imgUrl:string=myAppConfig.tmdb.imgUrl;
+  
+  constructor(private landingService:LandingService) {
+    this.landingService.getTopMoviesPosters()
+      this.landingService.getTopTvshowPosters()
+
+   
+
+    this.landingService.posterData.subscribe(data=>{
+      this.postersList=data;
+      console.log(data);
+      
+      console.log(this.postersList);
+    })
+   }
 
   ngOnInit(): void {
   }
